562d327029bea2adf9b41c51bd64e31e34002476
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-04 08:45:24 +0100

    :sparkles: Render api data to UI

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import axios from 'axios';
-
+import PlayerList from './PlayerList'
 export default class Home extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            players: []
+        }
+    }
+
+
     componentDidMount(){
         axios.get('http://localhost:5000/api/players')
         .then(response => {
             console.log(response.data);
+            this.setState({
+                players: response.data
+            })
         })
         .catch(error => console.log(error))
     }
 
     render(){
+        const {players} = this.state
         return(
-            <></>
+            <PlayerList players={players} />
         )
     }
 }
