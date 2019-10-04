f78c91e4483d3c9556941eedabc194e238b16fa7
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-04 08:36:34 +0100

    :sparkles: Fetch data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Home extends React.Component{
     componentDidMount(){
-        
+        axios.get('http://localhost:5000/api/players')
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => console.log(error))
     }
 
     render(){
-
+        return(
+            <></>
+        )
     }
 }
